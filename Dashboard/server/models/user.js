c60dccdfc93e1921d9fe1d628c961dea6e3a27bb
ilyapashayan <ilya.w.a.j@gmail.com>
2023-02-12 21:36:52 +0400

    user schema

---
+++ Dashboard/server/models/user.js
@@ -0,0 +1,27 @@
+import  mongoose from "mongoose";
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  role: {
+    type: String,
+    enum: ['admin', 'manager'],
+    required: true
+  }
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
